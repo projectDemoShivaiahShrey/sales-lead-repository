3eeb08a5a6e9d558b1a85827d2a712e473d8407c
Connor Hunter <clh7090@rit.edu>
2023-07-12 12:38:41 -0400

    Delete Springboot-backend/origin directory

--- Springboot-backend/origin/OriginRepository.java
+++
@@ -1,9 +0,0 @@
-package ups.hackathontest.origin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OriginRepository extends JpaRepository<OriginModel, Long> {
-
-}
