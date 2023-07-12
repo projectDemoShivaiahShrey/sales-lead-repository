fe1d0702d1a34081b123070afe77f37cdfd710e8
clh7090 <clh7090@rit.edu>
2023-07-12 12:35:09 -0400

    fix.

--- Springboot-backend/hackathon/src/main/java/ups/hackathon/destination/DestinationRepository.java
+++
@@ -1,9 +0,0 @@
-package ups.hackathon.destination;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DestinationRepository extends JpaRepository<DestinationModel, Long> {
-
-}
--- Springboot-backend/hackathon/src/main/java/ups/hackathon/trackingInfo/TrackingInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package ups.hackathon.trackingInfo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrackingInfoRepository extends JpaRepository<TrackingInfoModel, String> {
-
-}
