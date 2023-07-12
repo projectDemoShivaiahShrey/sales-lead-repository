8e4819cd01b81bf3377dcbe83a5cdd99f0c75cf8
Connor Hunter <clh7090@rit.edu>
2023-07-12 12:39:05 -0400

    Delete Springboot-backend/carrier directory

--- Springboot-backend/carrier/CarrierRepository.java
+++
@@ -1,9 +0,0 @@
-package ups.hackathontest.carrier;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarrierRepository extends JpaRepository<CarrierModel, Long> {
-
-}
