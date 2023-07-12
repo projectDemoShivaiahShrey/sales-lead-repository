7d861ffd67a5efb9ed4b018b77b3f2d0d9fc56b4
Buttless <sauravchala@gmail.com>
2023-07-12 15:28:28 -0400

    Origin controller and service

--- Springboot-backend/hackathon/src/main/java/ups/hackathon/origin/OriginController.java
+++ Springboot-backend/hackathon/src/main/java/ups/hackathon/origin/OriginController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/")
 public class OriginController {
@@ -15,4 +18,16 @@ public class OriginController {
         this.originService = originService;
     }
 
+
+
+    @GetMapping("/origins")
+    public List<OriginModel> getAllOrigins(){
+        return originService.getAllOrigins();
+    }
+
+
+    @GetMapping("/origins/{originID}")
+    public Optional<OriginModel> getOriginById(@PathVariable Long originID){
+        return originService.getOriginById(originID);
+    }
 }
