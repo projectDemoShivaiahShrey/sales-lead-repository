9c2fa87ef6b2e0de6dbd5da5fbd8f202942b4088
Connor Hunter <clh7090@rit.edu>
2023-07-12 12:39:13 -0400

    Delete Springboot-backend/client directory

--- Springboot-backend/client/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package ups.hackathontest.client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<ClientModel, Long> {
-
-}
